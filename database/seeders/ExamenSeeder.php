038ca32834168ab194a89dd14ae6b635f051d853
ilandbz <ilandbz@gmail.com>
2023-09-20 10:54:39 -0500

    redes

--- database/seeders/ExamenSeeder.php
+++ database/seeders/ExamenSeeder.php
@@ -22,7 +22,7 @@ class ExamenSeeder extends Seeder
             'hora_inicio'   => '00:00',
             'hora_fin'      => '23:00',
             'duracion' => 10,
-            'estado'    => 1
+            'estado'    => 0
         ]);
         Examen::firstOrCreate(
         [
@@ -31,7 +31,7 @@ class ExamenSeeder extends Seeder
             'fecha' => '2023-09-20',
             'hora_inicio'   => '00:00',
             'hora_fin'      => '23:00',
-            'duracion' => 1,
+            'duracion' => 10,
             'estado'    => 1
         ]);
         Examen::firstOrCreate(
